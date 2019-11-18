3643ed678b187d87083f372fce677fafd8972e7c
Xiuming Lee <lxmcoder@gmail.com>
2019-11-18 10:59:55 +0800

    添加Eureka服务注册中心

---
+++ shop-eureka-server/src/main/java/cn/xiuminglee/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package cn.xiuminglee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author Xiuming Lee
+ * @Description
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
